d98a4231095315a791560ef0338c7a36abfce4bd
Thiago Martins <thinomar@gmail.com>
2020-10-14 01:39:17 -0300

    Backend

---
+++ backend/src/routes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import multer from "multer";
+import OrphanageController from "./controllers/OrphanageController";
+import uploadConfig from './config/upload';
+
+const routes = Router();
+const upload = multer(uploadConfig);
+
+routes.get('/orphanages', OrphanageController.index);
+routes.get('/orphanages/:id', OrphanageController.show);
+routes.post('/orphanages', upload.array('images'), OrphanageController.create);
+
+export default routes;
---
+++ backend/src/views/orphanage_view.ts
@@ -0,0 +1,17 @@
+import Orphanage from "../models/Orphanage";
+import imagesView from './image_view';
+
+export default {
+  render(orphanage: Orphanage) {
+    return {
+      name: orphanage.name, 
+      latitude: orphanage.latitude,
+      longitude: orphanage.longitude,
+      about: orphanage.about,
+      instructions: orphanage.instructions,
+      opening_hours: orphanage.opening_hours,
+      open_on_weekends: orphanage.open_on_weekends,
+      images: imagesView.renderMany(orphanage.images)
+    }
+  }
+}
